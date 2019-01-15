08ddcc75c734f5fab816224ede97292b046e292b
Steve Brush <steve.brush@blackbaud.com>
2019-01-15 09:40:26 -0500

    Added `screenshotName` to visual tests. (#27)

--- e2e/alert.e2e-spec.ts
+++ e2e/alert.e2e-spec.ts
@@ -7,12 +7,16 @@ describe('Alert', () => {
   it('should match previous screenshot', (done) => {
     SkyHostBrowser.get('visual/alert');
     SkyHostBrowser.setWindowBreakpoint('lg');
-    expect('.sky-alert-visual').toMatchBaselineScreenshot(done);
+    expect('.sky-alert-visual').toMatchBaselineScreenshot(done, {
+      screenshotName: 'alert-lg'
+    });
   });
 
   it('should match previous screenshot (screen: xs)', (done) => {
     SkyHostBrowser.get('visual/alert');
     SkyHostBrowser.setWindowBreakpoint('xs');
-    expect('.sky-alert-visual').toMatchBaselineScreenshot(done);
+    expect('.sky-alert-visual').toMatchBaselineScreenshot(done, {
+      screenshotName: 'alert-xs'
+    });
   });
 });
--- e2e/text-highlight.e2e-spec.ts
+++ e2e/text-highlight.e2e-spec.ts
@@ -4,15 +4,21 @@ import {
 } from '@skyux-sdk/e2e';
 
 describe('Text highlight', () => {
-  it('should match previous screenshot', (done) => {
+  beforeEach(() => {
     SkyHostBrowser.get('visual/text-highlight');
+  });
+
+  it('should match previous screenshot', (done) => {
     SkyHostBrowser.setWindowBreakpoint('lg');
-    expect('.sky-text-highlight-demo').toMatchBaselineScreenshot(done);
+    expect('.sky-text-highlight-demo').toMatchBaselineScreenshot(done, {
+      screenshotName: 'text-highlight-lg'
+    });
   });
 
   it('should match previous screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/text-highlight');
     SkyHostBrowser.setWindowBreakpoint('xs');
-    expect('.sky-text-highlight-demo').toMatchBaselineScreenshot(done);
+    expect('.sky-text-highlight-demo').toMatchBaselineScreenshot(done, {
+      screenshotName: 'text-highlight-xs'
+    });
   });
 });
