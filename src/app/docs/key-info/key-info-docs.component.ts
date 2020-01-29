c1e892f7e3ef32ee3931d03c4649507d358f01fb
Denise Pena <denise.pena@blackbaud.me>
2020-01-29 16:59:03 -0600

    Added key info component design guidelines

---
+++ src/app/docs/key-info/key-info-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-key-info-docs',
+  templateUrl: './key-info-docs.component.html'
+})
+export class KeyInfoDocsComponent { }
