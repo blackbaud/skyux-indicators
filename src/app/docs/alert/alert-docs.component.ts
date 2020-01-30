9372a3300ccc6a4b16d0cd3bb0dbe4aa379aa762
John Lyons <John.Lyons@blackbaud.me>
2020-01-30 08:20:37 -0500

    Alert docs

--- src/app/docs/alert/alert-docs.component.ts
+++ src/app/docs/alert/alert-docs.component.ts
@@ -2,8 +2,33 @@ import {
   Component
 } from '@angular/core';
 
+import {
+  SkyDocsDemoControlPanelChange,
+  SkyDocsDemoControlPanelRadioChoice
+} from '@skyux/docs-tools';
+
 @Component({
   selector: 'app-alert-docs',
   templateUrl: './alert-docs.component.html'
 })
-export class AlertDocsComponent { }
+export class AlertDocsComponent {
+  public typeChoices: SkyDocsDemoControlPanelRadioChoice[] = [
+    { value: 'danger', label: 'Danger' },
+    { value: 'info', label: 'Info' },
+    { value: 'success', label: 'Success' },
+    { value: 'warning', label: 'Warning' }
+  ];
+
+  public demoSettings: any = {};
+
+  public onDemoSelectionChange(change: SkyDocsDemoControlPanelChange): void {
+    this.demoSettings.closed = false;
+
+    if (change.closable !== undefined) {
+      this.demoSettings.closable = change.closable;
+    }
+    if (change.alertType) {
+      this.demoSettings.type = change.alertType;
+    }
+  }
+}
