44ab4af48f08cf462eb4d952a889731f8718cef2
Steve Brush <steve.brush@blackbaud.com>
2020-02-20 14:57:24 -0500

    [rc] Remove components and directives from exports API (#64)

--- src/app/public/index.ts
+++ src/app/public/index.ts
@@ -1,35 +1,22 @@
-export * from './modules/alert/alert.component';
 export * from './modules/alert/alert.module';
 
-export * from './modules/chevron/chevron.component';
 export * from './modules/chevron/chevron.module';
 
-export * from './modules/help-inline/help-inline.component';
 export * from './modules/help-inline/help-inline.module';
 
-export * from './modules/icon/icon.component';
 export * from './modules/icon/icon.module';
 
-export * from './modules/key-info/key-info-label.component';
-export * from './modules/key-info/key-info-value.component';
-export * from './modules/key-info/key-info.component';
 export * from './modules/key-info/key-info.module';
 
-export * from './modules/label/label.component';
 export * from './modules/label/label.module';
 
-export * from './modules/text-highlight/text-highlight.directive';
 export * from './modules/text-highlight/text-highlight.module';
 
-export * from './modules/tokens/token.component';
-export * from './modules/tokens/tokens.component';
 export * from './modules/tokens/tokens.module';
 export * from './modules/tokens/types/token';
 export * from './modules/tokens/types/token-selected-event-args';
 export * from './modules/tokens/types/tokens-message';
 export * from './modules/tokens/types/tokens-message-type';
 
-export * from './modules/wait/wait-page.component';
-export * from './modules/wait/wait.component';
 export * from './modules/wait/wait.module';
 export * from './modules/wait/wait.service';
