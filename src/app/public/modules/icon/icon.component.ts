989d3678843c66519980549ea98df225fd3d8650
Paul Crowder <paul.crowder@blackbaud.com>
2020-04-02 14:17:44 -0400

    Fixed typo

--- src/app/public/modules/icon/icon.component.ts
+++ src/app/public/modules/icon/icon.component.ts
@@ -22,7 +22,7 @@ export class SkyIconComponent {
   public icon: string;
 
   /**
-   * Specifies the type of icon to display. Specyfing `fa` will display a FontAwesome icon,
+   * Specifies the type of icon to display. Specifying `fa` will display a Font Awesome icon,
    * while specifying `skyux` will display an icon from the custom SKY UX icon font. Note that
    * the custom SKY UX icon font is currently in beta.
    */
