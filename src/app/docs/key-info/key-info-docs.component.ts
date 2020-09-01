f5080ae35893d31c57adf2c0c005064cacdfc410
Alex Kingman <alex.kingman@blackbaud.me>
2020-09-01 16:17:25 -0400

    Added demo and code example for key info component

--- src/app/docs/key-info/key-info-docs.component.ts
+++ src/app/docs/key-info/key-info-docs.component.ts
@@ -1,9 +1,25 @@
 import {
   Component
 } from '@angular/core';
+import { SkyDocsDemoControlPanelRadioChoice, SkyDocsDemoControlPanelChange } from '@skyux/docs-tools';
 
 @Component({
   selector: 'app-key-info-docs',
   templateUrl: './key-info-docs.component.html'
 })
-export class KeyInfoDocsComponent {}
+export class KeyInfoDocsComponent {
+
+  public layout: string = 'vertical';
+
+  public layoutChoices: SkyDocsDemoControlPanelRadioChoice[] = [
+    { value: 'vertical', label: 'Vertical' },
+    { value: 'horizontal', label: 'Horizontal' }
+  ];
+
+  public onDemoSelectionChange(event: SkyDocsDemoControlPanelChange): void {
+    if (event.layout !== undefined) {
+      this.layout = event.layout;
+    }
+  }
+
+}
