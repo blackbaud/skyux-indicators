2711798c4b9bfb0915fcaada59479b662a927be6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-01-05 09:00:26 -0500

    Install/setup @nrwl/cypress

---
+++ projects/indicators-showcase/cypress/integration/alert.spec.ts
@@ -0,0 +1,10 @@
+describe('Alert', () => {
+  beforeEach(() => {
+    cy.visit('/visual/alert');
+    cy.viewport(1200, 800);
+  });
+
+  it('should match basic screenshot', () => {
+    cy.get('.sky-alert-visual').screenshot();
+  });
+});
